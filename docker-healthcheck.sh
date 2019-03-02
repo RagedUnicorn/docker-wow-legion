49482161066a54bde9f9e97eb9368968dbe79e02
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2019-03-02 17:59:03 +0100

    Simplify healthcheck by only checking 8085/8086 ports

--- docker-healthcheck.sh
+++ docker-healthcheck.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-if nc -z localhost 8081 && nc -z localhost 8085; && nc -z localhost 8086; && nc -z localhost 1119; then
+if nc -z localhost 8085 && nc -z localhost 8086; then
 	exit 0
 fi
 
